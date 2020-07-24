28afaee968c209377a195eb7bc6ddc923dac7817
Cleiton <omehe.cleiton@hotmail.com>
2020-07-24 11:41:24 -0300

    Começo implementação

--- dotnet/tdd/Using_NUnit/NumerosRomanos.cs
+++ dotnet/tdd/Using_NUnit/NumerosRomanos.cs
@@ -1,6 +1,16 @@
+using System;
+
 namespace Using_NUnit
 {
     public class NumerosRomanos
     {
+        public bool Converter(string numero)
+        {
+            if(!String.IsNullOrEmpty(numero))
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
--- dotnet/tdd/Using_NUnit/NumerosRomanos_Test.cs
+++ dotnet/tdd/Using_NUnit/NumerosRomanos_Test.cs
@@ -4,6 +4,18 @@ namespace Using_NUnit
 {
     [TestFixture]
     public class NumerosRomanos_Test
-    {        
+    {
+        [Test]
+        public void EntenderOsSimbolos()
+        {
+            // Arrange
+            NumerosRomanos romanos = new NumerosRomanos();
+            
+            // Act
+            bool verificado = romanos.Converter("V");
+            
+            // Assert
+            Assert.IsTrue(verificado);
+        }        
     }
 }
